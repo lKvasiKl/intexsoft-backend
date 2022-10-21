0d94ab1c4bedd7090cbb9c9bedb7ebed93b9d4aa
Kiryl Vasko <kirill.vasko.2003@gmail.com>
2022-10-21 22:29:55 +0300

    Initial commit

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 const start = () => {
     try {
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
