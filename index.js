610d264ac169799044ede0621245e49ef9937b06
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-10-31 12:57:07 +0300

    Add route

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import * as dotenv from 'dotenv'
 dotenv.config();
 import express from 'express'
 
+import router from "./app/router/index.js";
+
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+app.use("/api", router);
 
 const start = () => {
     try {
