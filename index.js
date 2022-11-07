31e7884bfdf77118a9301745b6340fb32e9bc77f
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-08 01:53:04 +0300

    Edit main index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-import * as dotenv from 'dotenv'
-dotenv.config();
-import express from 'express'
-
-import router from "./app/router/index.js";
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./app/util/database');
+require('./app/models/index');
+const bodyParser = require('body-parser');
+const router = require('./app/routes/index');
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use("/api", router);
 
-const start = () => {
+const start = async () => {
     try {
+        sequelize.authenticate().catch(err => console.log(err));
+        await sequelize.sync({force:true});
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (err) {
         console.log(err);
