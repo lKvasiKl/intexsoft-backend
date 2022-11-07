311c40222d2da7fff47a5bb3a704dc210f0fe9fd
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-08 01:54:30 +0300

    Edit authController

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,5 +1,36 @@
-export const login = async (req, res, next) => {
-    //const {email, password} = req.body;
-    return res.json("It is login route");
-}
+const authService = require('../services/authService');
+
+const register = async (req, res) => {
+    const {email, password, lastName, firstName} = req.body;
+    if (!email || !password) {
+        res.status(400).send();
+    }
+
+    const result = await authService.register({email, password, lastName, firstName});
+    if (result) {
+        return res.status(201).json({result});
+    }
+};
+
+const login = async (req, res) => {
+    const {email, password} = req.body;
+    const result = authService.login({email, password});
+
+    if (result) {
+        const {accessToken, refreshToken} = result;
+        return res.json({accessToken, refreshToken});
+    }
+};
+
+const refresh = async (req, res) => {
+    const {refreshToken} = req.body;
+    const accessToken = authService.refresh({refreshToken});
+    return res.json({accessToken: accessToken});
+};
+
+module.exports = {
+    register,
+    login,
+    refresh
+};
 
