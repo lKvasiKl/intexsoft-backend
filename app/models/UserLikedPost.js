b08383494ef548f60afa0f1d4cb179ccc1bd86b6
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-13 00:29:01 +0300

    Delete sequelize and models

--- app/models/UserLikedPost.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../util/database');
-const {DataTypes} = require('sequelize');
-
-const UserLikedPost = sequelize.define('users_liked_posts', {
-    user_id: {type: DataTypes.INTEGER, foreignKey: true, allowNull: false},
-    post_id: {type: DataTypes.INTEGER, foreignKey: true, allowNull: false}
-});
-
-module.exports = UserLikedPost;
