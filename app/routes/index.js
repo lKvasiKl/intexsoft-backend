c046b0c1d77dbc5a25d2a4daedd50bcf3d8ef1f2
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-13 00:31:21 +0300

    Edit routes index file

--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authRouter = require('./authRouter');
-const userRouter = require('./userRouter');
-
-router.use("/auth", authRouter);
-router.use("/user", userRouter);
-
-module.exports = router;
