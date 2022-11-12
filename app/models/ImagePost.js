d6a03833645727c655474f39d0184119f1288a04
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-13 00:29:01 +0300

    Delete sequelize and models

--- app/models/ImagePost.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../util/database');
-const {DataTypes} = require('sequelize');
-
-const ImagePost = sequelize.define('images_posts', {
-    image_id: {type: DataTypes.INTEGER, foreignKey: true, allowNull: false},
-    post_id: {type: DataTypes.INTEGER, foreignKey: true, allowNull: false}
-});
-
-module.exports = ImagePost;
