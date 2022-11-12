7045cda99c5954a68253ea82f16d984b8c768e30
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-13 00:35:14 +0300

    Create user repository

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,62 @@
+const {PrismaClient} = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const getByEmail = async (email) => {
+    return await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    });
+};
+
+const getById = async (id) => {
+    return await prisma.user.findUnique({
+        where: {
+            id: +id
+        }
+    });
+};
+
+const create = async (userData) => {
+    const {email, hashPassword, firstName, lastName} = userData;
+    return await prisma.user.create({
+        data: {
+            email: email,
+            hashPassword: hashPassword,
+            firstName: firstName,
+            lastName: lastName
+        }
+    });
+};
+
+const update = async (id, userData) => {
+    const {email, hashPassword, firstName, lastName} = userData;
+    return await prisma.user.update({
+        where: {
+            id: id
+        },
+        data: {
+            email: email,
+            hashPassword: hashPassword,
+            firstName: firstName,
+            lastName: lastName
+        }
+    });
+};
+
+const remove = async id => {
+    return await prisma.user.delete({
+        where: {
+            id: id
+        }
+    });
+};
+
+module.exports = {
+    getByEmail,
+    getById,
+    create,
+    update,
+    remove
+}
