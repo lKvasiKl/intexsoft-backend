df7c11bfac2cdf3c7eca5c126e7766a8ca1ba82c
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-13 00:34:57 +0300

    Edit userRouter

---
+++ src/routes/userRouter.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const authMiddleware = require('../middlewares/checkAuth');
+const {getUser, getCurrentUser, updateCurrentUser, deleteCurrentUser} = require('../controllers/userController');
+
+router.get(
+    "/:id",
+    getUser
+);
+
+router.get(
+    "/",
+    authMiddleware,
+    getCurrentUser
+);
+
+router.patch(
+    "/",
+    authMiddleware,
+    updateCurrentUser
+);
+
+router.delete(
+    "/",
+    authMiddleware,
+    deleteCurrentUser
+);
+
+
+module.exports = router;
