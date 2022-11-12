61541eae8fe36aec2b67404b32c8be844afc4b82
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-13 00:27:09 +0300

    Edit authRouter

--- app/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {register, login, refresh} = require('../controllers/authController');
-const AuthValidator = require('../validation/authValidator');
-
-router.post("/register", AuthValidator.register, register);
-router.post("/login", AuthValidator.login, login);
-router.get("/refresh", refresh);
-
-module.exports = router;
