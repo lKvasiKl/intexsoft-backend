bc03cccf3cee0aa20cf61f1dc0fc996a67dc9017
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-13 00:39:22 +0300

    Update auth service

---
+++ src/services/authService.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const userService = require('./userService');
+const crypt = require('../helpers/crypt');
+const jwt = require('../helpers/jwt');
+const {removePassword} = require('../helpers/user')
+
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+const ACCESS_TOKEN_LIFETIME = process.env.ACCESS_TOKEN_LIFETIME;
+const REFRESH_TOKEN_LIFETIME = process.env.REFRESH_TOKEN_LIFETIME;
+
+const createTokensPair = (userData) => {
+    const {id} = userData;
+    const accessToken = jwt.generate(
+        {id},
+        JWT_ACCESS_SECRET,
+        {expiresIn: ACCESS_TOKEN_LIFETIME}
+    );
+
+    const refreshToken = jwt.generate(
+        {},
+        JWT_REFRESH_SECRET,
+        {expiresIn: REFRESH_TOKEN_LIFETIME}
+    )
+
+    return {accessToken, refreshToken};
+}
+
+const register = async (userData) => {
+    return userService
+        .create(userData)
+        .then((user) => removePassword(user))
+        .catch((e) => console.log(e));
+};
+
+const login = async (userData) => {
+    const {email, password} = userData;
+    const foundedUser = await userService.getByEmail(email);
+
+    if (!foundedUser) {
+        throw new Error("User with this email doesn't exists.")
+    }
+    const isPasswordValid = crypt.compare(
+        password,
+        foundedUser.hashPassword
+    )
+
+    if (!isPasswordValid) {
+        throw new Error("Password not valid.");
+    }
+
+    return createTokensPair(removePassword(foundedUser));
+};
+
+const refresh = ({refreshToken}) => {
+    const isValid = jwt.validate(refreshToken, JWT_REFRESH_SECRET);
+
+    if (!isValid) {
+        throw new Error("Refresh token not valid.");
+    }
+
+    const {id} = jwt.parse(refreshToken);
+
+    return createTokensPair({id});
+};
+
+module.exports = {
+    register,
+    login,
+    refresh
+};
