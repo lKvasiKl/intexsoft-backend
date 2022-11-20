e1470885331b62176a3db592e83fda7fa50a2fb7
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-20 17:26:45 +0300

    Change index directory

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const authRouter = require('./authRouter');
 const userRouter = require('./userRouter');
+const checkAuth = require('../middlewares/checkAuth');
 
 router.use("/auth", authRouter);
-router.use("/user", userRouter);
+router.use("/user", checkAuth, userRouter);
 
 module.exports = router;
