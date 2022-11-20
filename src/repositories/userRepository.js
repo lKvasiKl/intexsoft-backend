ea14ff77848e9acb8dbbe0b00334ba3fda5bbd93
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-20 17:29:47 +0300

    Edit user repository

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,6 +1,4 @@
-const {PrismaClient} = require('@prisma/client');
-
-const prisma = new PrismaClient();
+const prisma = require('../prisma/feed');
 
 const getByEmail = async (email) => {
     return await prisma.user.findUnique({
