1692a1f269003ee68207dbb338a5511c06024b7f
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-20 17:30:12 +0300

    Edit user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,37 +1,46 @@
 const userRepository = require('../repositories/userRepository');
 const crypt = require('../helpers/crypt');
 
-const getByEmail = async (email) => {
-    return await userRepository.getByEmail(email);
+const getByEmail = (email) => {
+    return userRepository.getByEmail(email);
 }
 
-const getById = async id => {
-    return await userRepository.getById(id);
+const getById = async (id) => {
+    const user = await userRepository.getById(id);
+
+    if (!user) {
+        throw new Error(`User with id ${id} not found.`);
+    }
+
+    return user;
 };
 
 const create = async (userData) => {
     const {email, password} = userData;
-    const userToRegister = getByEmail(email);
+
+    const userToRegister = await getByEmail(email);
+
     if (!userToRegister) {
         return new Error("User with this email already exists.");
     }
 
     userData.hashPassword = crypt.hash(password);
 
-    return await userRepository.create(userData);
+    return userRepository.create(userData);
 };
 
-const update = async (id, userData) => {
+const update = (id, userData) => {
     const {password} = userData;
+
     if (password) {
         userData.hashPassword = crypt.hash(password);
     }
 
-    return await userRepository.update(id, userData);
+    return userRepository.update(id, userData);
 };
 
-const remove = async id => {
-    return await userRepository.remove(id);
+const remove = (id) => {
+    return userRepository.remove(id);
 };
 
 module.exports = {
