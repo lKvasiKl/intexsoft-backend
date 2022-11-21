7ca12e70c6095552a050a29295c7e19e0799794c
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-21 23:58:34 +0300

    New routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const authRouter = require('./authRouter');
 const userRouter = require('./userRouter');
+const postRouter = require('./postRouter');
+const commentRouter = require('./commentRouter');
 const checkAuth = require('../middlewares/checkAuth');
 
 router.use("/auth", authRouter);
 router.use("/user", checkAuth, userRouter);
+router.use("/post", checkAuth, postRouter);
+router.use("/comment", checkAuth, commentRouter);
 
 module.exports = router;
