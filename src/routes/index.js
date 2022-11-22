f5ab9735c76cb08559dfd38ee7cfa45e0940e1ce
Kirill Vasko <kirill.vasko.2003@gmail.com>
2022-11-22 11:30:11 +0300

    Add new route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const authRouter = require('./authRouter');
 const userRouter = require('./userRouter');
 const postRouter = require('./postRouter');
+const likeRouter = require('./likedPostRouter')
 const commentRouter = require('./commentRouter');
 const checkAuth = require('../middlewares/checkAuth');
 
 router.use("/auth", authRouter);
 router.use("/user", checkAuth, userRouter);
 router.use("/post", checkAuth, postRouter);
+router.use("/like", checkAuth, likeRouter);
 router.use("/comment", checkAuth, commentRouter);
 
 module.exports = router;
